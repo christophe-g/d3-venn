9b1e6de16723c4120c88b36efa6ae5e080cfcf68
christophe-g <christophe.geiser@gmail.com>
2015-12-08 10:58:50 +0100

    change export_name

--- end.js
+++ end.js
@@ -1,12 +1,12 @@
-(function(target, cname, name) {
+(function(target, export_name, name) {
   if (target) {
-    target[name] = this[cname] && this[cname][name];
+    target[name] = this[export_name] && this[export_name][name];
 
-    for (var k in this[cname]) {
+    for (var k in this[export_name]) {
       if (k != name) {
-        target[name][k] = this[cname][k];
+        target[name][k] = this[export_name][k];
       }
     }
-    delete this[cname];
+    delete this[export_name];
   }
 }(this.d3 && this.d3.layout, 'd3_venn', 'venn'));
